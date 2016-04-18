3af08750149a9312d5f2ba32c74c04ae33f710a0
Alvaro Fernandez <afernandez@cookinapps.io>
2016-04-18 12:28:39 +0200

    Session example: sessions-login-and-cart

---
+++ examples/sessions-login-and-cart/src/business/cart.cc
@@ -0,0 +1,117 @@
+/**
+  * Copyright (c) <2016> HTML Puzzle Team <htmlpuzzleteam@gmail.com>
+  *
+  * This source code is licensed under the MIT license.
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in
+  * all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  *
+  */
+
+#include "cart.h"
+
+namespace business{
+
+  std::unique_ptr<granada::cache::SharedMapCacheDriver> Cart::cache_ = std::unique_ptr<granada::cache::SharedMapCacheDriver>(new granada::cache::SharedMapCacheDriver());
+
+
+  Cart::Cart(granada::http::session::Session* session){
+    session_ = session;
+  }
+
+
+  int Cart::Count(){
+    return cache_->Length(GetHash());
+  }
+
+
+  int Cart::Add(const std::string product_id, int quantity){
+    std::string hash = GetHash();
+
+    // first check if this product is not already added, if so sum quantities.
+    std::string stored_quantity_str = cache_->Read(hash,product_id);
+
+    if (!stored_quantity_str.empty()){
+      int stored_quantity = std::atoi(stored_quantity_str.c_str());
+      quantity += stored_quantity;
+    }
+
+    cache_->Write(hash,product_id,std::to_string(quantity));
+
+    return Count();
+  }
+
+
+  int Cart::Edit(const std::string product_id, int quantity){
+    std::string hash = GetHash();
+
+    if (quantity > 0){
+      cache_->Write(hash,product_id,std::to_string(quantity));
+    }else{
+      cache_->Destroy(hash,product_id);
+    }
+    return Count();
+  }
+
+
+  int Cart::Remove(const std::string product_id){
+    cache_->Destroy(GetHash(),product_id);
+    return 0;
+  }
+
+
+  void Cart::Destroy(){
+    cache_->Destroy(GetHash());
+  }
+
+
+  std::string Cart::List(){
+    std::unordered_map<std::string,std::string> products = cache_->GetProperties(GetHash());
+    std::string list_str = "[";
+
+    // loop throw all sessions.
+    for ( auto it = products.begin(); it != products.end(); ++it ){
+      if (it != products.begin()){
+        list_str += ",";
+      }
+      list_str += "{\"id\":\"" + it->first + "\",\"quantity\":\"" + it->second + "\"}";
+    }
+
+    list_str += "]";
+    return list_str;
+  }
+
+
+  void Cart::SwitchToUser(){
+    std::string old_hash = "CART_" + session_->GetToken();
+    if (cache_->Length(old_hash) > 0){
+      std::unordered_map<std::string,std::string> products = cache_->GetProperties(old_hash);
+      cache_->Write(GetHash(),products);
+    }
+    cache_->Destroy(old_hash);
+  }
+
+
+  std::string Cart::GetHash(){
+    if (session_->roles()->Is("USER")){
+      return "CART_" + session_->roles()->GetProperty("USER", "username");
+    }else{
+      return "CART_"+session_->GetToken();
+    }
+  }
+}
---
+++ examples/sessions-login-and-cart/src/http/controller/cart_controller.cc
@@ -0,0 +1,160 @@
+/**
+  * Copyright (c) <2016> HTML Puzzle Team <htmlpuzzleteam@gmail.com>
+  *
+  * This source code is licensed under the MIT license.
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in
+  * all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  *
+  * Controlle for managing cart.
+  *
+  */
+
+#include "cart_controller.h"
+
+namespace granada{
+  namespace http{
+    namespace controller{
+      CartController::CartController(utility::string_t url)
+      {
+        m_listener_ = std::unique_ptr<http_listener>(new http_listener(url));
+        m_listener_->support(methods::GET, std::bind(&CartController::handle_get, this, std::placeholders::_1));
+        m_listener_->support(methods::PUT, std::bind(&CartController::handle_put, this, std::placeholders::_1));
+        m_listener_->support(methods::POST, std::bind(&CartController::handle_post, this, std::placeholders::_1));
+      }
+
+      void CartController::handle_get(web::http::http_request request)
+      {
+
+        web::http::http_response response;
+
+        granada::http::session::SimpleSession simple_session(request,response);
+        business::Cart cart(&simple_session);
+
+        auto paths = uri::split_path(uri::decode(request.relative_uri().path()));
+        if (!paths.empty()){
+          std::string name = paths[0];
+
+          if(name == "count"){
+
+            // returns the number of products added to the cart.
+            int count = cart.Count();
+            response.set_body("{\"count\":\"" + std::to_string(count) + "\"}");
+
+          }else if (name == "list"){
+
+            // returns a list with the products added to the cart but only
+            // if the user is logged in.
+            if (simple_session.roles()->Is("USER")){
+              response.set_body("{\"data\":" + cart.List() + "}");
+            }else{
+              response.set_body("{\"data\":[]}");
+            }
+          }
+        }
+
+        response.set_status_code(status_codes::OK);
+        response.headers().add(U("Content-Type"), "text/json; charset=utf-8");
+
+        request.reply(response);
+      }
+
+      void CartController::handle_put(web::http::http_request request)
+      {
+        web::http::http_response response;
+        granada::http::session::SimpleSession simple_session(request,response);
+        business::Cart cart(&simple_session);
+
+        std::string product_id = "";
+        int quantity = 0;
+        std::string body = request.extract_utf8string(true).get();
+        std::unordered_map<std::string,std::string> parsed_query = granada::http::parser::ParseQueryString(body);
+
+        auto it = parsed_query.find("id");
+        if (it != parsed_query.end()){
+          product_id.assign(it->second);
+        }
+
+        auto it2 = parsed_query.find("quantity");
+        if (it2 != parsed_query.end()){
+          quantity = std::atoi(it2->second.c_str());
+        }
+
+        int count = 0;
+        if (product_id.empty()){
+
+          // invalid product id, do not add the product,
+          // just return the number of products already added.
+          count = cart.Count();
+
+        }else{
+
+          // adds a product to the cart.
+          count = cart.Add(product_id,quantity);
+
+        }
+
+        response.set_status_code(status_codes::OK);
+        response.headers().add(U("Content-Type"), "text/json; charset=utf-8");
+        response.set_body("{\"count\":\"" + std::to_string(count) + "\"}");
+        request.reply(response);
+      }
+
+      void CartController::handle_post(web::http::http_request request)
+      {
+        web::http::http_response response;
+        granada::http::session::SimpleSession simple_session(request,response);
+        business::Cart cart(&simple_session);
+
+        std::string product_id = "";
+        int quantity = 0;
+        std::string body = request.extract_utf8string(true).get();
+        std::unordered_map<std::string,std::string> parsed_query = granada::http::parser::ParseQueryString(body);
+
+        auto it = parsed_query.find("id");
+        if (it != parsed_query.end()){
+          product_id.assign(it->second);
+        }
+
+        auto it2 = parsed_query.find("quantity");
+        if (it2 != parsed_query.end()){
+          quantity = std::atoi(it2->second.c_str());
+        }
+
+        int count = 0;
+        if (product_id.empty()){
+
+          // invalid product id, do not modify the product,
+          // just return the number of products already added.
+          count = cart.Count();
+
+        }else{
+
+          // modify the product already added in the cart.
+          count = cart.Edit(product_id,quantity);
+
+        }
+
+        response.set_status_code(status_codes::OK);
+        response.headers().add(U("Content-Type"), "text/json; charset=utf-8");
+        response.set_body("{\"count\":\"" + std::to_string(count) + "\"}");
+        request.reply(response);
+      }
+    }
+  }
+}

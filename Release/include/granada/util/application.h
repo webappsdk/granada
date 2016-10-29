6eca48e7e40a22055e1448161352036ed0a38f1f
Alvaro Fernandez <afernandez@cookinapps.io>
2016-10-29 19:16:22 +0200

    Add application.cpp and in examples.

--- Release/include/granada/util/application.h
+++ Release/include/granada/util/application.h
@@ -37,7 +37,7 @@
 #elif _WIN32
     #define WIN32_LEAN_AND_MEAN
     #define NOMINMAX
-	#include <windows.h>
+  #include <windows.h>
 #else
   #include <linux/limits.h>
 #endif
@@ -68,42 +68,7 @@ namespace granada{
        * Returns the path of the application.
        * @return Path.
        */
-      static const std::string& get_selfpath(){
-        if (selfpath.empty()){
-            #ifdef __APPLE__
-              int ret;
-              pid_t pid; 
-              char pathbuf[PROC_PIDPATHINFO_MAXSIZE];
-              pid = getpid();
-              ret = proc_pidpath (pid, pathbuf, sizeof(pathbuf));
-              if ( ret > 0 ) {
-                std::string totalPath = std::string(pathbuf);
-                selfpath = totalPath.substr(0,totalPath.find_last_of("/"));
-              }
-            #elif _WIN32
-              std::vector<wchar_t> pathBuf;
-              DWORD copied = 0;
-
-              do {
-                pathBuf.resize(pathBuf.size() + MAX_PATH);
-                copied = GetModuleFileName(0, (PWSTR)&pathBuf.at(0), pathBuf.size());
-              } while (copied >= pathBuf.size());
-
-              pathBuf.resize(copied);
-              selfpath = utility::conversions::to_utf8string(std::wstring(pathBuf.begin(), pathBuf.end()));
-              selfpath = selfpath.substr(0, selfpath.find_last_of("\\"));
-            #else
-              char buff[PATH_MAX];
-              ssize_t len = ::readlink("/proc/self/exe", buff, sizeof(buff)-1);
-              if (len != -1) {
-                buff[len] = '\0';
-                std::string totalPath = std::string(buff);
-                selfpath = totalPath.substr(0,totalPath.find_last_of("/"));
-              }
-            #endif
-          }
-        return selfpath;
-      };
+      const std::string& get_selfpath();
 
 
       /**
@@ -111,13 +76,7 @@ namespace granada{
        * @param  name Name of the property to retrieve.
        * @return      Value of the property.
        */
-      static const std::string GetProperty(const std::string& name){
-        if (property_file_ == NULL){
-          std::string configuration_file_path = get_selfpath() + "/server.conf";
-          property_file_ = std::unique_ptr<granada::util::file::PropertyFile>(new granada::util::file::PropertyFile(configuration_file_path));
-        }
-        return property_file_->GetProperty(name);
-      };
+      const std::string GetProperty(const std::string& name);
 
 
       /**
